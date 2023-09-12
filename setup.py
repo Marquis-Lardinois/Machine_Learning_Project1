06b39fd7d6ac1ef3d392077680e3e6204626c4f7
Marquis Lardinois <marquislard@gmail.com>
2023-09-11 20:23:29 -0700

    Commit 2

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Machine_Learning_Project1',
+    version='0.0.1',
+    author='Marquis',
+    author_email='Marquislard@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy', 'seaborn']
+)
