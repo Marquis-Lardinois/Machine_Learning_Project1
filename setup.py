6339105d9d8e3e89cc851865ccd85b5ac471983f
Marquis Lardinois <marquislard@gmail.com>
2023-09-11 20:36:55 -0700

    Initial Commit

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function returns requirements list
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
 setup(
     name='Machine_Learning_Project1',
@@ -6,5 +16,5 @@ setup(
     author='Marquis',
     author_email='Marquislard@gmail.com',
     packages=find_packages(),
-    install_requires=['pandas', 'numpy', 'seaborn']
-)
+    install_requires=get_requirements('requirements.txt')
+    )
